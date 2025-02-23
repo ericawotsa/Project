6553a143ab17b2c4ce019d7e87895fcf2ddfbbaf
ericawotsa <ericawotsa5@gmail.com>
2025-02-24 01:25:41 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Remove experimental options if they cause issues
 };
 
 module.exports = nextConfig;
